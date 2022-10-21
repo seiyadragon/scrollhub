12e1438623a7afa39c9be7f6fa16df5c663a9b41
seiyadragon <34778012+seiyadragon@users.noreply.github.com>
2022-10-21 01:44:00 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,7 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      
-      <Script
-          data-ad-client = "ca-pub-9062696682480550"
-          async = {true}
-          strategy= "beforeInteractive"
-          src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        />
-
+    
       <Component {...pageProps} />
 
     </div>
