b01f2f040fff777264bb23ece3dbb777173a3c52
seiyadragon <34778012+seiyadragon@users.noreply.github.com>
2022-11-24 13:50:10 -0600

    0.1.2

--- pages/books/[id].js
+++ pages/books/[id].js
@@ -3,37 +3,42 @@ import { createClient } from "@supabase/supabase-js";
 import { LogoImage, NavBar } from "..";
 
 const supabase = createClient(
-  "https://psffjnyfrkdpfafzdiwg.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzZmZqbnlmcmtkcGZhZnpkaXdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg4Nzc1MzAsImV4cCI6MTk4NDQ1MzUzMH0.NPyCh_ZYc4nOzKsQtwF6GK8q6c8yjpkxZty5et4LzIE"
+	"https://psffjnyfrkdpfafzdiwg.supabase.co",
+	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzZmZqbnlmcmtkcGZhZnpkaXdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njg4Nzc1MzAsImV4cCI6MTk4NDQ1MzUzMH0.NPyCh_ZYc4nOzKsQtwF6GK8q6c8yjpkxZty5et4LzIE"
 );
 
 export async function getServerSideProps(context) {
-  var { data, error } = await supabase
-    .from("Books")
-    .select("*")
-    .eq("id", context.query.id);
+	var { data, error } = await supabase
+		.from("Books")
+		.select("*")
+		.eq("id", context.query.id);
 
-  var text = await (await fetch(data[0].formats["text/html"])).text();
-  text = text.replace(/<style([\S\s]*?)>([\S\s]*?)<\/style>/g, '<--No More Styles-->')
+	var text = await (await fetch(data[0].formats["text/html"])).text();
 
-  return {
-    props: { 
-      book: data[0],
-      bookText: text,
-    }
-  };
+	text = text.replace(/<style([\S\s]*?)>([\S\s]*?)<\/style>/g, "<!--I Set The Style-->");
+	text = text.replaceAll("<hr>", "<!--No More Hr-->");
+
+	return {
+		props: {
+			book: data[0],
+			bookText: text,
+		},
+	};
 }
 
 export default function Book({ book, bookText }) {
-  return (
-    <main>
-      <NavBar title={book.title}/>
-      <section className={styles.content}>
-        <LogoImage />
-        <section className={styles.bookText}>
-          <section className={styles.bookTextInner} dangerouslySetInnerHTML={{__html: bookText}}/>
-        </section>
-      </section>
-    </main>
-  );
+	return (
+		<main>
+			<NavBar title={book.title} />
+			<section className={styles.content}>
+				<LogoImage />
+				<section className={styles.bookText}>
+					<section
+						className={styles.bookTextInner}
+						dangerouslySetInnerHTML={{ __html: bookText }}
+					/>
+				</section>
+			</section>
+		</main>
+	);
 }
